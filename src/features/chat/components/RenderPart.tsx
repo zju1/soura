c6eb776e4c45a55f026b88bd4cc1c19db3fe4a4b
Azamat Jumabaev <singledeveloper63@gmail.com>
2025-05-15 16:02:33 +0500

    Add domain research

--- src/features/chat/components/RenderPart.tsx
+++ src/features/chat/components/RenderPart.tsx
@@ -2,18 +2,28 @@ import { Markdown } from "@/components/markdown";
 import { type Message } from "@ai-sdk/react/";
 
 type MessagePart = NonNullable<Message["parts"]>[number];
-
 export function RenderPart(props: MessagePart) {
   switch (props.type) {
     case "text":
       return <Markdown>{props.text}</Markdown>;
     case "tool-invocation": {
       if (props.toolInvocation.state !== "result") {
-        return (
-          <h1 className="font-grotesk text-blue-600 animate-pulse">
-            Searching...
-          </h1>
-        );
+        switch (props.toolInvocation.toolName) {
+          case "singleSupplierResearch": {
+            return (
+              <h1 className="font-grotesk text-zinc-800 animate-pulse">
+                Analyzing...
+              </h1>
+            );
+          }
+          default: {
+            return (
+              <h1 className="font-grotesk text-zinc-800 animate-pulse">
+                Searching...
+              </h1>
+            );
+          }
+        }
       }
       return;
     }
